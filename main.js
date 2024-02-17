47871d3d5c980d3e3b76168eda3b27f9ae1abf78
Hkhan222 <160311998+Hkhan222@users.noreply.github.com>
2024-02-17 09:04:21 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question1
+console.log('Hello world');
+// Question2
+var myName = 'Hammad khan';
+var message = 'is a nice person';
+console.log(myName + "" + message);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Question1
+//console.log('Hello world');
+
+// Question2
+//let myName ='Hammad khan';
+//let message ='is a nice person';
+//console.log(myName+""+message);
